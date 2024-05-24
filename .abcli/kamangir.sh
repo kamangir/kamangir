fb6137f33bdcf4ed0d01d5593dfa9158d9e7a591
kamangir <arash@kamangir.net>
2024-05-23 19:03:46 -0700

    kamangir-3.50.1 update - kamangir/bolt#746

--- .abcli/kamangir.sh
+++ .abcli/kamangir.sh
@@ -4,47 +4,13 @@ function kamangir() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ]; then
-        kamangir update "$@"
+        kamangir_update "$@"
         return
     fi
 
-    if [ "$task" == "init" ]; then
-        abcli_init kamangir "${@:2}"
-        return
-    fi
-
-    if [[ "|pylint|pytest|test|" == *"|$task|"* ]]; then
-        abcli_${task} plugin=kamangir,$2 \
-            "${@:3}"
-        return
-    fi
-
-    if [ "$task" == "update" ]; then
-        local options=$2
-
-        if [ $(abcli_option_int "$options" help 0) == 1 ]; then
-            abcli_show_usage "kamangir update [push]" \
-                "update README.md."
-            return
-        fi
-
-        local do_push=$(abcli_option_int "$options" push 0)
-
-        python3 -m kamangir \
-            update \
-            "${@:3}"
-
-        if [[ "$do_push" == 1 ]]; then
-            abcli_git kamangir push \
-                "$(python3 -m kamangir version) update"
-        else
-            abcli_git kamangir status ~all
-        fi
-
-        return
-    fi
-
-    python3 -m kamangir "$@"
+    abcli_generic_task \
+        plugin=kamangir,task=$task \
+        "${@:2}"
 }
 
 abcli_source_path \
--- kamangir/__init__.py
+++ kamangir/__init__.py
@@ -4,4 +4,4 @@ ICON = "📜"
 
 DESCRIPTION = f"{ICON} github/kamangir"
 
-VERSION = "3.50.1"
+VERSION = "3.51.1"
