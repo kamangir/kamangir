bf910f0320f20bad04414ce1bbf4e921878e157e
kamangir <arash@kamangir.net>
2024-07-21 23:48:06 -0700

    Refactor - kamangir/bolt#746

--- kamangir/__init__.py
+++ kamangir/__init__.py
@@ -4,6 +4,6 @@ ICON = "📜"
 
 DESCRIPTION = f"{ICON} github/kamangir."
 
-VERSION = "3.117.1"
+VERSION = "3.118.1"
 
 REPO_NAME = "kamangir"
--- kamangir/tests/test_build.py
+++
@@ -1,5 +0,0 @@
-from kamangir.README import build
-
-
-def test_build():
-    assert build()
