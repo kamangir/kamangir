1cb0266190f85b39984d8be24155f5f4f8834b4e
kamangir <arash@kamangir.net>
2025-01-09 00:07:04 -0800

    refactor - kamangir/bolt#746

--- kamangir/.abcli/kamangir.sh
+++ kamangir/.abcli/kamangir.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 function kamangir() {
-    local task=$(abcli_unpack_keyword $1 help)
+    local task=$(abcli_unpack_keyword $1 version)
 
     abcli_generic_task \
         plugin=kamangir,task=$task \
